5bae219c61e54ea6d806e6e3900b14fdbc5892cd
qepting91 <64446420+qepting91@users.noreply.github.com>
2023-08-05 09:47:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,21 @@
 import streamlit as st
-import home as home
-import movie_suggester as movie_suggester
-import story_generator as story_generator
-import decoration_scraper as decoration_scraper
-
-# If 'page' is not in the session_state, set it to 'Home'
-if 'page' not in st.session_state:
-    st.session_state.page = 'Home'
+import home
+import movie_suggester
+import story_generator
+import decoration_scraper
 
 PAGES = {
-    "Home": home,
-    "History/Recipes/DIY": decoration_scraper,
-    "Halloween Movie Suggester": movie_suggester,
-    "Spooky Story Generator": story_generator,
+    "🏠 Home Sweet Haunted Home": home,
+    "👻 History/Recipes/DIY": decoration_scraper,
+    "🎬 Spooky Movie Suggester": movie_suggester,
+    "📚 Spooky Story Generator": story_generator
 }
 
 def main():
-    st.markdown("""
-    <style>
-    @import url('https://fonts.googleapis.com/css2?family=Creepster&display=swap');
-    body {
-        font-family: 'Creepster', cursive;
-    }
-    </style>
-    """, unsafe_allow_html=True)
-    st.sidebar.title('Navigation')
-    st.session_state.page = st.sidebar.radio("Go to", list(PAGES.keys()), index=list(PAGES.keys()).index(st.session_state.page))
-    page = PAGES[st.session_state.page]
-
-    with st.spinner(f"Loading {st.session_state.page} ..."):
-        page.app()
+    st.sidebar.title('🎃 Navigation')
+    page_selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+    page = PAGES[page_selection]
+    page.app()
 
 if __name__ == "__main__":
     main()
